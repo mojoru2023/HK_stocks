92e21f0a52c98af65780d1d2cfa04000dc39cd2a
Greenbirch2007 <291109028@qq.com>
2020-08-16 09:25:57 +0800

    change data resource

--- 恒生指数成分股模型/hk50_Daily.py
+++ 恒生指数成分股模型/hk50_Daily.py
@@ -1,6 +1,4 @@
 
-
-
 import datetime
 import time
 
@@ -71,7 +69,7 @@ class HKPool_M(object):
         html  = self.page_request()
         element = etree.HTML(html)
 
-        now_price = element.xpath('//*[@id="spFP"]/div[1]/span[1]/text()')
+        now_price = element.xpath('//*[@id="quote_header"]/span/text()')
         for item in now_price:
             big_list.append(item)
         return big_list
@@ -114,7 +112,7 @@ if __name__ == '__main__':
 
 
     for it in HK50:
-        url = 'http://gu.qq.com/hk{0}/gp'.format(it)
+        url = 'http://stockpage.10jqka.com.cn/HK{0}/'.format(it[1:])
         print(url)
         hksp = HKPool_M(url)# 这里把请求和解析都进行了处理
         hksp.page_parse_()
@@ -123,30 +121,30 @@ if __name__ == '__main__':
     ff_l.append((f_tup))
     print(ff_l)
     insertDB(ff_l)
+#
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- # create table HK_mons(id int not null primary key auto_increment, hk00001   float,hk00002   float,hk00003   float,hk00005   float,hk00006   float,hk00011   float,hk00012   float,hk00016   float,hk00017   float,hk00019   float,hk00027   float,hk00066   float,hk00083   float,hk00101   float,hk00151   float,hk00175   float,hk00267   float,hk00288   float,hk00386   float,hk00388   float,hk00669   float,hk00688   float,hk00700   float,hk00762   float,hk00823   float,hk00857   float,hk00883   float,hk00939   float,hk00941   float,hk01038   float,hk01044   float,hk01088   float,hk01093   float,hk01109   float,hk01113   float,hk01177   float,hk01299   float,hk01398   float,hk01928   float,hk01997   float,hk02007   float,hk02018   float,hk02313   float,hk02318   float,hk02319   float,hk02382   float,hk02388   float,hk02628   float,hk03328   float,hk03988   float, LastTime timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP ) engine=InnoDB  charset=utf8;
-
-
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#  # create table HK_mons(id int not null primary key auto_increment, hk00001   float,hk00002   float,hk00003   float,hk00005   float,hk00006   float,hk00011   float,hk00012   float,hk00016   float,hk00017   float,hk00019   float,hk00027   float,hk00066   float,hk00083   float,hk00101   float,hk00151   float,hk00175   float,hk00267   float,hk00288   float,hk00386   float,hk00388   float,hk00669   float,hk00688   float,hk00700   float,hk00762   float,hk00823   float,hk00857   float,hk00883   float,hk00939   float,hk00941   float,hk01038   float,hk01044   float,hk01088   float,hk01093   float,hk01109   float,hk01113   float,hk01177   float,hk01299   float,hk01398   float,hk01928   float,hk01997   float,hk02007   float,hk02018   float,hk02313   float,hk02318   float,hk02319   float,hk02382   float,hk02388   float,hk02628   float,hk03328   float,hk03988   float, LastTime timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP ) engine=InnoDB  charset=utf8;
+#
+#
